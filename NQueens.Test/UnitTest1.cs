6926528874778a71320d8d1b7cbce99df65267ac
chile109 <chile109@gmail.com>
2023-03-09 17:07:03 +0800

    add: unit test SolveNQueens_Input8_GetAnswer92

--- NQueens.Test/UnitTest1.cs
+++ NQueens.Test/UnitTest1.cs
@@ -4,14 +4,17 @@ namespace NQueens.Test;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public void SolveNQueens_Input8_GetAnswer92()
     {
-        Assert.Pass();
+        // Arrange
+        var inputN = 8;
+        int[,] board = new int[inputN, inputN];
+
+        // Act
+        int numSolutions = RedDoor.NQueensSolver.SolveNQueens(board, 0, inputN, false);
+
+        // Assert
+        Assert.AreEqual(92, numSolutions);
     }
 }
